5b20fbc19423d0bcfbc2875978edc51c4fc1c88b
K-Developer <krndvlpr@gmail.com>
2022-03-11 21:57:53 +0900

    Initial Commit

---
+++ FCDiary/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FCDiary
+//
+//  Created by joe on 03/11/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
