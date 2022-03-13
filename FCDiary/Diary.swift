8106dd6cd3eb3fbaf7690c281e92935038e54238
K-Developer <krndvlpr@gmail.com>
2022-03-13 20:39:13 +0900

    diary register form logic & delegate for passing data done

---
+++ FCDiary/Diary.swift
@@ -0,0 +1,15 @@
+//
+//  Diary.swift
+//  FCDiary
+//
+//  Created by joe on 03/13/2022.
+//
+
+import Foundation
+
+struct Diary {
+    var title: String
+    var contents: String
+    var date: Date
+    var isStar: Bool
+}
--- FCDiary/ViewController.swift
+++ FCDiary/ViewController.swift
@@ -9,11 +9,25 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    private var diaryList = [Diary]()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if let wireDiaryViewController = segue.destination as? WriteDiaryViewController {
+            wireDiaryViewController.delegate = self
+        }
+    }
+}
 
-
+extension ViewController: WriteDiaryViewDelegate {
+    func didSelectRegister(diary: Diary) {
+        self.diaryList.append(diary)
+    }
 }
 
